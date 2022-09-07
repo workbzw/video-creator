15599ed10b0395135e824257dd53a9440be2decd
bzw <workbzw@outlook.com>
2022-09-07 23:21:01 +0800

    []

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -6,11 +6,11 @@ const app = express();
 
 
 app.get("/list", (req, res) => {
-    createVideo((filePath: string) => {
-        console.log("createVideo.callback:" + filePath)
+    createVideo((filePathCallback: string) => {
+        console.log("createVideo.callback:" + filePathCallback)
+        let filePath = path.join(__dirname, "../video/output/example.mp4");
+        res.download(filePath);
     })
-    let filePath = path.join(__dirname, "../video/output/example.mp4");
-    res.download(filePath);
 });
 app.listen("3000", () => {
     console.log("=========服务启动了，在3000端口=========");
