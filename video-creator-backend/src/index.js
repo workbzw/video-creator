d073c4334d82337bc4a6724376ef83b5b191575b
bzw <workbzw@outlook.com>
2022-09-16 17:58:05 +0800

    [fix]

--- video-creator-backend/src/index.js
+++ video-creator-backend/src/index.js
@@ -7,7 +7,7 @@ app.get("/video/create", (req, res) => {
     const imgUrl = req.query.url
     let str = "上述视频发布后引发众多网友热议，目前已有近8万点赞，评论数接近5万条。有网友科普道，“基本上所有益生菌都是从人体提取出来的，大部分是粪便。不过不要膈应，因为培养后和粪便完全没丁点关系了。”也有网友表示，“虽然知道没什么，但是感觉过不去心里那道坎。”还有网友则调侃称：“能不能找点瘦子的益生菌。”\n" + "\n" + "极目新闻记者注意到，此前已有网友在网络平台晒出了这款酸奶，产品包装上的醒目位置标注了“中国发明专利菌株”“巴马益生菌”“鼠李糖乳杆菌hsryfm1301”等字样。一位网友在晒出这款酸奶时配文写道：“这个巴马益生菌感觉之前在论文里看到过，是从巴马长寿老人……”"
     textToVoice(str, (mp3File) => {
-        createVideo(imgUrl, (fileName) => {
+        createVideo(mp3File,imgUrl, (fileName) => {
             res.send(fileName)
             console.log("createVideo.callback:" + fileName)
         })
