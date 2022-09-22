9cda2d6e291bbebad46a7a5b13572884ecc31484
bzw <workbzw@outlook.com>
2022-09-22 17:24:43 +0800

    [fix]

--- video-creator-frontend/src/page/common/view/ImageUpload.tsx
+++ video-creator-frontend/src/page/common/view/ImageUpload.tsx
@@ -16,7 +16,7 @@ export const ImageUpload = () => {
         multiple: true,
         maxCount: 16,
         accept: 'image/*',
-        action: 'http://127.0.0.1:3001/api/v1/upload',
+        action: 'http://110.42.242.63:3001/api/v1/upload',
         headers: {
             authorization: 'authorization-text',
         },
